91b374dd1f017c6096877959f9420f270228bd4c
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2018-06-19 00:05:02 +0200

    tmp

--- apps/example-one/src/app/app.component.ts
+++ apps/example-one/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { RouterStateSnapshot } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'e1-root',
@@ -6,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  route$: Observable<RouterStateSnapshot>;
+
+  constructor(private store: Store) {
+    this.route$ = this.store.select(route => route)
+      .pipe(
+        tap(route => console.log(route))
+      );
+  }
 
 }
